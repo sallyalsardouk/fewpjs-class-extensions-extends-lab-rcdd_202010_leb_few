739444a6bc1cb15c67c87df58b209a0b81c9fd84
sally al sardouk <sally.sardouk98@gmail.com>
2020-12-15 10:30:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Your code here
+class Polygon{
+  constructor(arr){
+    this.arr=arr
+  }
+  get countSides(){ return this.arr.length
+}
+get perimeter(){
+  let sum=0
+  forEach (count in this.arr){
+    sum+=count
+  } 
+  return sum
+}
+}
+class Triangle extends Polygon{
+  this.arr=arr
+}
