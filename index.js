cc6faa17540c13c46fe98da52102189843b1a4ec
sally al sardouk <sally.sardouk98@gmail.com>
2020-12-15 10:37:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,7 +18,15 @@ class Triangle extends Polygon{
     let arr1=this.arr[0]
     let arr2=this.arr[1]
     let arr3=this.arr[2]
-    return (arr1+arr2>arr3)
+    return ( (arr1+arr2>arr3) &&(arr1+arr3>arr2) &&(arr2+arr3>arr1))
     }
+  }
+class Square extends Polygon(){
+  get isValid(){
+    let arr1=this.arr[0]
+    let arr2=this.arr[1]
+    let arr3=this.arr[2]
+    let arr4=this.arr[3]
+    retun (arr1=arr2=arr3=arr4)
   }
 }
